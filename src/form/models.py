92b0e35347b58e81aedf1c50589f7df501cc26da
Marek Prochazka <m.prochazka2002@gmail.com>
2021-09-14 10:39:33 +0200

    init request model

--- src/form/models.py
+++ src/form/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
+from uuid import uuid4
+
 
 # Create your models here.
+
+
+class Request(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    x_created = CreationDateTimeField("x_created")
+    x_modified = ModificationDateTimeField("x_modified")
+
+    requester_name = models.CharField(max_length=64, verbose_name='Requester name')
+    sleepover_date = models.DateField(verbose_name='Sleepover date')
+    num_persons = models.IntegerField(verbose_name='Number of persons')
+    accepted = models.BooleanField(default=False, verbose_name='Accepted')
